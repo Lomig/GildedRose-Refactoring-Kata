2ca4615f9f3357533f23d87cfff57594cd6d5ea4
Lomig Enfroy <lomig@hey.com>
2021-04-11 01:03:43 +0200

    2a: Intercept one case from the big conditionnal

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -12,9 +12,13 @@ class GildedRose
     item.quality +=1 if item.sell_in < 0
   end
 
+  def backstage_update(item)
+  end
+
   def update_quality()
     @items.each do |item|
       next aged_brie_update(item) if item.name == "Aged Brie"
+      next backstage_update(item) if item.name == "Backstage passes to a TAFKAL80ETC concert"
 
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
