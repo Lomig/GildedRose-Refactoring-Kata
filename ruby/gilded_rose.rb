122754a640f3027b1b111084609c5fed2f71dd69
Lomig Enfroy <lomig@hey.com>
2021-04-11 03:06:55 +0200

    8: Turn GildedRose into a Factory

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,81 +1,81 @@
+class CommonItem
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+
+  def update
+    item.sell_in -= 1
+
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 0
+    
+    limit_quality
+    self
+  end
+
+  private
+
+  def limit_quality
+    return item.quality = 0 if item.quality < 0
+
+    item.quality = 50 if item.quality > 50
+  end
+end
+
+class AgedBrie < CommonItem
+  def update
+    item.sell_in -= 1
+
+    item.quality +=1
+    item.quality +=1 if item.sell_in < 0
+
+    limit_quality
+    self
+  end
+end
+
+class Backstage < CommonItem
+  def update
+    item.sell_in -= 1
+    item.quality = 0 and return self if item.sell_in < 0
+
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 10
+    item.quality += 1 if item.sell_in < 5
+
+    limit_quality
+    self
+  end
+end
+
+class Sulfuras < CommonItem
+  def update
+    self
+  end
+end
+
 class GildedRose
+  ITEM_CLASSES = {
+    "Aged Brie"                                 => AgedBrie,
+    "Sulfuras, Hand of Ragnaros"                => Sulfuras,
+    "Backstage passes to a TAFKAL80ETC concert" => Backstage
+  }
 
   def initialize(items)
-    @items = items
+    @items = items.map { |item| class_from(name: item.name).new(item) }
   end
 
   def update_quality
-    @items.each do |item|
-      case item.name
-      when "Aged Brie"
-        AgedBrie.new(item).update
-      when "Backstage passes to a TAFKAL80ETC concert"
-        Backstage.new(item).update
-      when "Sulfuras, Hand of Ragnaros"
-        Sulfuras.new(item).update
-      else
-        CommonItem.new(item).update
-      end
-    end
+    @items.each { |item| item.update }
   end
 
-  class CommonItem
-    attr_reader :item
-
-    def initialize(item)
-      @item = item
-    end
-
-
-    def update
-      item.sell_in -= 1
-
-      item.quality -= 1
-      item.quality -= 1 if item.sell_in < 0
-      
-      limit_quality
-      self
-    end
-
-    private
-
-    def limit_quality
-      return item.quality = 0 if item.quality < 0
-
-      item.quality = 50 if item.quality > 50
-    end
-  end
-
-  class AgedBrie < CommonItem
-    def update
-      item.sell_in -= 1
-
-      item.quality +=1
-      item.quality +=1 if item.sell_in < 0
-
-      limit_quality
-      self
-    end
-  end
-
-  class Backstage < CommonItem
-    def update
-      item.sell_in -= 1
-      item.quality = 0 and return self if item.sell_in < 0
-
-      item.quality += 1
-      item.quality += 1 if item.sell_in < 10
-      item.quality += 1 if item.sell_in < 5
-
-      limit_quality
-      self
-    end
-  end
-
-  class Sulfuras < CommonItem
-    def update
-      self
-    end
+  private
+  
+  def class_from(name:)
+    ITEM_CLASSES[name] || CommonItem
   end
 end
 
