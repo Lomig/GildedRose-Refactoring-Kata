89d4c7a0dc68cdca5e95e2855a74cfa90bbf8d9d
Lomig Enfroy <lomig@hey.com>
2021-04-11 02:43:55 +0200

    6: Transform #foo_suffixer into foo.suffixer

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,48 +4,79 @@ class GildedRose
     @items = items
   end
 
-  def aged_brie_update(item)
-    item.sell_in -= 1
-
-    item.quality +=1
-    item.quality +=1 if item.sell_in < 0
-    item.quality = 50 if item.quality > 50
-  end
-
-  def backstage_update(item)
-    item.sell_in -= 1
-    return item.quality = 0 if item.sell_in < 0
-
-    item.quality += 1
-    item.quality += 1 if item.sell_in < 10
-    item.quality += 1 if item.sell_in < 5
-    item.quality = 50 if item.quality > 50
-  end
-
-  def sulfuras_update(item)
-  end
-
-  def common_update(item)
-    item.sell_in -= 1
-
-    item.quality -= 1
-    item.quality -= 1 if item.sell_in < 0
-    item.quality = 0 if item.quality < 0
-  end
-
-  def update_quality()
+  def update_quality
     @items.each do |item|
       case item.name
       when "Aged Brie"
-        aged_brie_update(item)
+        AgedBrie.new(item).update
       when "Backstage passes to a TAFKAL80ETC concert"
-        backstage_update(item)
+        Backstage.new(item).update
       when "Sulfuras, Hand of Ragnaros"
-        sulfuras_update(item)
+        Sulfuras.new(item).update
       else
-        common_update(item)
+        Common.new(item).update
       end
     end
+  end
+
+  class Common
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def update
+      item.sell_in -= 1
+
+      item.quality -= 1
+      item.quality -= 1 if item.sell_in < 0
+      item.quality = 0 if item.quality < 0
+    end
+  end
+
+  class AgedBrie
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def update
+      item.sell_in -= 1
+
+      item.quality +=1
+      item.quality +=1 if item.sell_in < 0
+      item.quality = 50 if item.quality > 50
+    end
+  end
+
+  class Backstage
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def update
+      item.sell_in -= 1
+
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 10
+      item.quality += 1 if item.sell_in < 5
+      item.quality = 0 if item.sell_in < 0
+      item.quality = 50 if item.quality > 50
+    end
+  end
+
+  class Sulfuras
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def update; end
   end
 end
 
